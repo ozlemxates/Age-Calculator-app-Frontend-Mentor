02e17e04774fec25c3299d9845786396a401567f
Özlem Ateş <ozlemxatess@outlook.com>
2023-05-29 13:59:01 +0300

    Improved error validation handling

--- age-calculator-app.js
+++ age-calculator-app.js
@@ -9,7 +9,7 @@ function calculateAge() {
 
   let currentDate = new Date();
   let currentYear = currentDate.getFullYear();
-  let currentMonth = currentDate.getMonth() +1;
+  let currentMonth = currentDate.getMonth() + 1;
   let currentDay = currentDate.getDate();
 
   let years = currentYear - birthYear;
@@ -17,121 +17,132 @@ function calculateAge() {
   let days = currentDay - birthDay;
 
   if (currentMonth < birthMonth || (currentMonth === birthMonth && currentDay < birthDay)) {
-      years--;
-      months = 12 - birthMonth + currentMonth - 1;
-      if (currentDay < birthDay) {
-          months++;
-      }
+    years--;
+    months = 12 - birthMonth + currentMonth - 1;
+    if (currentDay < birthDay) {
+      months++;
+    }
   }
 
   if (currentDay < birthDay) {
-      let monthDays = getDaysInMonth(currentYear, currentMonth - 1);
-      days = monthDays - (birthDay - currentDay);
-      months--;
+    let monthDays = getDaysInMonth(currentYear, currentMonth - 1);
+    days = monthDays - (birthDay - currentDay);
+    months--;
 
-      if (months < 0) {
-          years--;
-          months += 12;
-      }
+    if (months < 0) {
+      years--;
+      months += 12;
+    }
   }
 
-  if (birthDay < 1 || birthDay > getDaysInMonth(currentYear, birthMonth - 1)) {
-    document.getElementById('errorDay').innerHTML = "Must be a valid day";
-    document.getElementById('day').classList.add('error-outline');
-    document.getElementById('h5-day').classList.add('error-color');
-    return;
-  } else {
-    document.getElementById('errorDay').innerHTML = "";
+if (
+    birthDay >= 1 &&
+    birthDay <= getDaysInMonth(currentYear, birthMonth - 1) &&
+    birthMonth >= 1 &&
+    birthMonth <= 12 &&
+    birthYear >= 1 &&
+    birthYear <= currentYear
+  ) {
+    document.getElementById('resultYear').innerHTML = years || '--';
+    document.getElementById('resultMonth').innerHTML = months || '--';
+    document.getElementById('resultDay').innerHTML = days || '--';
+  
+    document.getElementById('errorDay').innerHTML = '';
     document.getElementById('day').classList.remove('error-outline');
     document.getElementById('h5-day').classList.remove('error-color');
-  }
-  if (birthMonth < 1 || birthMonth > 12) {
-      document.getElementById('errorMonth').innerHTML = "Must be a valid month";
-      document.getElementById('month').classList.add('error-outline');
-      document.getElementById('h5-month').classList.add('error-color');
-      return;
-  } else {
-      document.getElementById('errorMonth').innerHTML = "";
-      document.getElementById('month').classList.remove('error-outline');
-      document.getElementById('h5-month').classList.remove('error-color');
-  }
+    document.getElementById('errorMonth').innerHTML = '';
+    document.getElementById('month').classList.remove('error-outline');
+    document.getElementById('h5-month').classList.remove('error-color');
+    document.getElementById('errorYear').innerHTML = '';
+    document.getElementById('year').classList.remove('error-outline');
+    document.getElementById('h5-year').classList.remove('error-color');
+    } else {
+        if (birthDay < 1 || birthDay > getDaysInMonth(currentYear, birthMonth - 1)) {
+        document.getElementById('errorDay').innerHTML = 'Must be a valid day';
+        document.getElementById('day').classList.add('error-outline');
+        document.getElementById('h5-day').classList.add('error-color');
+        } else {
+        document.getElementById('errorDay').innerHTML = '';
+        document.getElementById('day').classList.remove('error-outline');
+        document.getElementById('h5-day').classList.remove('error-color');
+        }
+        if (birthMonth < 1 || birthMonth > 12) {
+        document.getElementById('errorMonth').innerHTML = 'Must be a valid month';
+        document.getElementById('month').classList.add('error-outline');
+        document.getElementById('h5-month').classList.add('error-color');
+        } else {
+        document.getElementById('errorMonth').innerHTML = '';
+        document.getElementById('month').classList.remove('error-outline');
+        document.getElementById('h5-month').classList.remove('error-color');
+        }
+        if (birthYear < 1 || birthYear > currentYear) {
+        document.getElementById('errorYear').innerHTML = 'Must be a valid year';
+        document.getElementById('year').classList.add('error-outline');
+        document.getElementById('h5-year').classList.add('error-color');
+        } else {
+        document.getElementById('errorYear').innerHTML = '';
+        document.getElementById('year').classList.remove('error-outline');
+        document.getElementById('h5-year').classList.remove('error-color');
+        }
+    }
   
-  if (birthYear < 1 || birthYear > currentYear) {
-      document.getElementById('errorYear').innerHTML = "Must be a valid year";
-      document.getElementById('year').classList.add('error-outline');
-      document.getElementById('h5-year').classList.add('error-color');
-      return;
-  } else {
-      document.getElementById('errorYear').innerHTML = "";
-      document.getElementById('year').classList.remove('error-outline');
-      document.getElementById('h5-year').classList.remove('error-color');
-  }
-
-    document.getElementById('resultYear').innerHTML = years || "--";
-    document.getElementById('resultMonth').innerHTML = months || "--";
-    document.getElementById('resultDay').innerHTML = days || "--";
 }
 
 function getDaysInMonth(year, month) {
-    if (month === 1) { 
-        if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-            return 29; 
-        } else {
-            return 28; 
-        }
-    } else if (month === 7) { 
-        return 31;
+    if (month === 1) {
+      if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+        return 29;
+      } else {
+        return 28;
+      }
+    } else if (month === 7) {
+      return 31;
+    } else if (month % 2 === 0 && month !== 8) {
+      return 30;
     } else {
-        return 30 + ((month % 7) % 2); 
+      return 31;
     }
 }
 
-document.addEventListener('keyup', function(event) {
-    if (event.key === 'Enter') {
-        event.preventDefault(); 
-        const currentElement = event.target;
-        if (currentElement.id === 'year') {
-            const dayInput = document.getElementById('day');
-            if (dayInput.value === '') {
-                dayInput.focus();
-            } else {
-                const monthInput = document.getElementById('month');
-                if (monthInput.value === '') {
-                    monthInput.focus();
-                } else {
-                    calculateAge();
-                }
-            }
-        } else if (currentElement.id === 'month') {
-            const yearInput = document.getElementById('year');
-            if (yearInput.value === '') {
-                yearInput.focus();
-            } else {
-                const dayInput = document.getElementById('day');
-                if (dayInput.value === '') {
-                    dayInput.focus();
-                } else {
-                    calculateAge();
-                }
-            }
-        } else if (currentElement.id === 'day') {
-            const monthInput = document.getElementById('month');
-            if (monthInput.value === '') {
-                monthInput.focus();
-            } else {
-                calculateAge();
-            }
+document.addEventListener('keyup', function (event) {
+  if (event.key === 'Enter') {
+    const currentElement = event.target;
+    if (currentElement.id === 'year') {
+      const dayInput = document.getElementById('day');
+      if (dayInput.value === '') {
+        dayInput.focus();
+      } else {
+        const monthInput = document.getElementById('month');
+        if (monthInput.value === '') {
+          monthInput.focus();
+        } else {
+          calculateAge();
         }
-    }
-});
-
-document.addEventListener('keyup', function(event) {
-    if (event.key === 'Enter') {
+      }
+    } else if (currentElement.id === 'month') {
+      const yearInput = document.getElementById('year');
+      if (yearInput.value === '') {
+        yearInput.focus();
+      } else {
+        const dayInput = document.getElementById('day');
+        if (dayInput.value === '') {
+          dayInput.focus();
+        } else {
+          calculateAge();
+        }
+      }
+    } else if (currentElement.id === 'day') {
+      const monthInput = document.getElementById('month');
+      if (monthInput.value === '') {
+        monthInput.focus();
+      } else {
         calculateAge();
+      }
     }
+  }
 });
 
 calculateAge();
-getDaysInMonth();
 
-// bu versiona geri dön
+// bu versiyona dön
+
